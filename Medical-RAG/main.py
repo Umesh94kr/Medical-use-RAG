77878be72642cf91b35a69eee0cb8cf91438d28c
Umesh94kr <umeshkumar94628@gmail.com>
2025-12-01 01:01:40 +0530

    RAG 100%

---
+++ Medical-RAG/main.py
@@ -0,0 +1,16 @@
+from rag import RAG
+
+if __name__ == "__main__":
+    rag = RAG()
+
+    while True:
+        query = input("Put your query for medical bot: ")
+
+        if query.lower() == "quit":
+            print("Thank you for chatting :)")
+            break
+
+        print(f"Query: {query}")
+        response = rag.main(query)
+        print(f"Response: {response}")
+        print("-" * 80)
